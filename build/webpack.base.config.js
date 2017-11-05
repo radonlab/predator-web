e72aeb622394569a789440089fa9263a11eba567
skylerlee <skyler.ac.lee@gmail.com>
2017-11-05 17:49:33 +0800

    Add html template

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,10 +1,16 @@
 'use strict'
 const config = require('./config')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: config.distPath,
     filename: `${config.appName}.js`
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template/index.html'
+    })
+  ]
 }
