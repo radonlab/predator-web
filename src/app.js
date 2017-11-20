ca92818278cc56810915815ee686a4c6c3a6ccea
skylerlee <skyler.ac.lee@gmail.com>
2017-11-21 01:27:26 +0800

    Add app entry

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017, Skyler.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  React.createElement('h1', null, 'Hello world!'),
+  document.getElementById('app')
+)
