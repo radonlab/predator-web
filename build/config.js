61903ea090e778c82152b6ea63b32970f920d92a
skylerlee <skyler.ac.lee@gmail.com>
2017-11-21 15:32:13 +0800

    Use shorter file hash

--- build/config.js
+++ build/config.js
@@ -4,5 +4,5 @@ const path = require('path')
 
 module.exports = {
   destPath: path.resolve(__dirname, '../dist'),
-  targetName: '[name].js?v=[chunkhash]'
+  targetName: '[name].js?v=[chunkhash:8]'
 }
