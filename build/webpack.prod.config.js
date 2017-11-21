446757a2117517a91b181cfebf7164501702ac0c
skylerlee <skyler.ac.lee@gmail.com>
2017-11-21 12:20:07 +0800

    Update webpack production config

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,5 +1,15 @@
 'use strict'
 
+const config = require('./config')
 const baseConfig = require('./webpack.base.config')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
 
-module.exports = baseConfig
+module.exports = merge(baseConfig, {
+  output: {
+    filename: `${config.appName}.min.js`
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+})
