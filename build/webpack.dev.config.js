bae08cac9ba09d2cc1b764bc0038a8c965d86f34
skylerlee <skyler.ac.lee@gmail.com>
2017-11-21 09:28:38 +0800

    Add dev server start script

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,5 +1,10 @@
 'use strict'
 
 const baseConfig = require('./webpack.base.config')
+const merge = require('webpack-merge')
 
-module.exports = baseConfig
+module.exports = merge(baseConfig, {
+  devServer: {
+    port: 8090
+  }
+})
