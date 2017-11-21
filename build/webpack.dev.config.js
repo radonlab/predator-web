51156f7df3679f0ea9a27d4f24dae0cefa1cf83c
skylerlee <skyler.ac.lee@gmail.com>
2017-11-21 16:26:28 +0800

    Add dashboard start script

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -2,9 +2,13 @@
 
 const baseConfig = require('./webpack.base.config')
 const merge = require('webpack-merge')
+const DashboardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = merge(baseConfig, {
   devServer: {
     port: 8090
-  }
+  },
+  plugins: [
+    new DashboardPlugin()
+  ]
 })
