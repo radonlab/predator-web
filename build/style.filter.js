43b8c618d5a4e9814c92719c135e352726136567
skylerlee <skyler.ac.lee@gmail.com>
2017-11-22 21:19:53 +0800

    Update style filter

--- build/style.filter.js
+++ build/style.filter.js
@@ -1,9 +1,23 @@
 'use strict'
 
 const settings = require('./settings')
+const merge = require('webpack-merge')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+let patch = {
+  module: {
+    rules: []
+  },
+  plugins: []
+}
+
 module.exports = {
   filter (config) {
     if (settings.options.extractCSS) {
+      config = merge({
+        customizeArray (a, b, key) {
+        }
+      })(config, patch)
     }
     return config
   }
