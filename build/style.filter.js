210b63e0ce82306b4b5782823ac4cc8834f12791
skylerlee <skyler.ac.lee@gmail.com>
2017-11-22 21:56:54 +0800

    Update style filter

--- build/style.filter.js
+++ build/style.filter.js
@@ -4,6 +4,43 @@ const settings = require('./settings')
 const merge = require('webpack-merge')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+let extractPlugin = new ExtractTextPlugin({
+  filename: '[name].css?v=[chunkhash:8]'
+})
+
+function patchLoaders (loaders) {
+  loaders.shift()
+  return extractPlugin.extract({
+    fallback: 'style-loader',
+    use: loaders
+  })
+}
+
+function isStyleRule (rule) {
+  if (rule.test && rule.test.test) {
+    if (rule.test.test('a.css') ||
+        rule.test.test('a.scss')) {
+      return true
+    }
+  }
+  return false
+}
+
+function patchRules (rules) {
+  for (let i = 0; i < rules.length; i++) {
+    let rule = rules[i]
+    if (isStyleRule(rule) && Array.isArray(rule.use)) {
+      rules[i].use = patchLoaders(rule.use)
+    }
+  }
+  return rules
+}
+
+function patchPlugins (plugins) {
+  plugins.push(extractPlugin)
+  return plugins
+}
+
 let patch = {
   module: {
     rules: []
@@ -16,6 +53,12 @@ module.exports = {
     if (settings.options.extractCSS) {
       config = merge({
         customizeArray (a, b, key) {
+          if (key === 'module.rules') {
+            return patchRules(a)
+          }
+          if (key === 'plugins') {
+            return patchPlugins(a)
+          }
         }
       })(config, patch)
     }
