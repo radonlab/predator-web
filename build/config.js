4fea2bb01b4bc45afdd32f2ab35f94f78f77f0a1
skylerlee <skyler.ac.lee@gmail.com>
2017-11-22 20:36:51 +0800

    Add extractCSS option

--- build/config.js
+++ build/config.js
@@ -5,5 +5,8 @@ const path = require('path')
 module.exports = {
   srcPath: path.resolve(__dirname, '../src'),
   destPath: path.resolve(__dirname, '../dist'),
-  targetName: '[name].js?v=[chunkhash:8]'
+  targetName: '[name].js?v=[chunkhash:8]',
+  options: {
+    extractCSS: false
+  }
 }
--- build/style.filter.js
+++ build/style.filter.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const config = require('./config')
 module.exports = {
   filter (config) {
     return config
