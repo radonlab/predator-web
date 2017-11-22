0a7aafc67cf2672d4ba320091b5b5c58ffd2f865
skylerlee <skyler.ac.lee@gmail.com>
2017-11-23 02:30:11 +0800

    Extend React Component with `binds' method

--- src/core/bootstrap.js
+++ src/core/bootstrap.js
@@ -3,3 +3,15 @@
  * Use of this source code is governed by the MIT license that can be
  * found in the LICENSE file.
  */
+
+import React from 'react'
+
+React.Component.prototype.binds = function (methodNames) {
+  for (let i = 0; i < methodNames.length; i++) {
+    let name = methodNames[i]
+    if (typeof this[name] !== 'function') {
+      throw new TypeError(`Failed to bind context to ${name}`)
+    }
+    this[name] = this[name].bind(this)
+  }
+}
