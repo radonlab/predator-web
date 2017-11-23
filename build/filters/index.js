0a03df5d57ef965bb885f666c0922a626f3d1414
skylerlee <skyler.ac.lee@gmail.com>
2017-11-23 17:26:46 +0800

    Update filters

--- build/filters/index.js
+++ build/filters/index.js
@@ -1,7 +1,26 @@
 'use strict'
 
-module.exports = function (config) {
+function loadFilter (name) {
+  try {
+    return require(`./${name}.filter`)
+  } catch (e) {
+    return null
+  }
+}
+
+function applyFilters (config) {
   return function (filterNames) {
+    for (var i = 0; i < filterNames.length; i++) {
+      let name = filterNames[i]
+      let filter = loadFilter(name)
+      if (filter && typeof filter === 'function') {
+        config = filter.filter(config)
+      } else {
+        throw new Error('Failed to apply filter: ' + name)
+      }
+    }
     return config
   }
 }
+
+module.exports = applyFilters
