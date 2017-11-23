2871a45bc684258e6f4bc0fd5139b4eb4cd8368a
skylerlee <skyler.ac.lee@gmail.com>
2017-11-23 10:46:56 +0800

    Add hot module replacement
    Ref: https://webpack.js.org/guides/hot-module-replacement/

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,15 +1,19 @@
 'use strict'
 
 const baseConfig = require('./webpack.base.config')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const DashboardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = merge(baseConfig, {
   devtool: 'eval-source-map',
   devServer: {
-    port: 8090
+    port: 8090,
+    hot: true
   },
   plugins: [
-    new DashboardPlugin()
+    new DashboardPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 })
