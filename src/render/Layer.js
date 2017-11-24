0f1b2d32f88155a83ae65efc8cd2f0e7334ab6f0
skylerlee <skyler.ac.lee@gmail.com>
2017-11-24 17:27:47 +0800

    Update layer

--- src/render/Layer.js
+++ src/render/Layer.js
@@ -4,8 +4,32 @@
  * found in the LICENSE file.
  */
 
+import Shape from './Shape'
+
+function assertType (inst, type) {
+  if (!(inst instanceof type)) {
+    throw new TypeError('Expect instance of ' + type.name)
+  }
+}
+
 class Layer {
   constructor () {
+    this._children = []
+  }
+
+  add (shape) {
+    assertType(shape, Shape)
+    this._children.push(shape)
+  }
+
+  remove (shape) {
+    assertType(shape, Shape)
+    let i = this._children.indexOf(shape)
+    if (i > -1) {
+      return this._children.splice(i, 1)[0]
+    } else {
+      return null
+    }
   }
 }
 
