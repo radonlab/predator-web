af606947f7bd4ff751e47dc16b31a084d6cde712
skylerlee <skyler.ac.lee@gmail.com>
2017-11-24 22:20:32 +0800

    Update layer draw method

--- src/render/Layer.js
+++ src/render/Layer.js
@@ -36,6 +36,9 @@ class Layer implements Drawable {
   }
 
   draw (ctx: CanvasRenderingContext2D) {
+    for (let i = 0; i < this._children.length; i++) {
+      this._children[i].draw(ctx)
+    }
   }
 }
 
