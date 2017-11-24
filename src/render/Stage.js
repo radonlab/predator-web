511c743baf625990800ad89a51396a75e074e23c
skylerlee <skyler.ac.lee@gmail.com>
2017-11-24 16:07:28 +0800

    Update stage

--- src/render/Stage.js
+++ src/render/Stage.js
@@ -7,9 +7,22 @@
 class Stage {
   constructor (canvas) {
     this.canvas = canvas
+    this.context = this._initContext()
   }
 
-  initContext () {
+  _initContext () {
+    // init canvas size
+    let css = getComputedStyle(this.canvas)
+    let width = parseInt(css.width, 10)
+    let height = parseInt(css.height, 10)
+    this.resize(width, height)
+    // get context
+    return this.canvas.getContext('2d')
+  }
+
+  resize (w, h) {
+    this.canvas.width = w
+    this.canvas.height = h
   }
 
   renderFrame () {
