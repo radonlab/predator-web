7816077a8099cfefc39054c4cd71c227e69d9176
skylerlee <skyler.ac.lee@gmail.com>
2017-11-25 21:28:38 +0800

    Optimize code

--- src/speech/Backdrop.js
+++ src/speech/Backdrop.js
@@ -8,6 +8,8 @@ import {Stage, Shape, Layer} from '@/render'
 
 const headWidth = 280
 const headHeight = 160
+const headPadH = 10
+const headPadV = 15
 
 class DroidHead extends Shape {
   constructor (x, y) {
@@ -26,28 +28,26 @@ class DroidHead extends Shape {
     let cx1 = this.x + 0.2 * headWidth
     let cy0 = this.y - 0.2 * headHeight
     let cy1 = this.y + 0.1 * headHeight
-    const padh = 10
-    const padv = 15
     ctx.clearRect(x0, y0, headWidth, headHeight)
     ctx.beginPath()
-    ctx.moveTo(x0 + padh, y0 + padv)
+    ctx.moveTo(x0 + headPadH, y0 + headPadV)
     ctx.bezierCurveTo(
       cx0, y0,
       cx1, y0,
-      x1 - padh, y0 + padv)
+      x1 - headPadH, y0 + headPadV)
     ctx.bezierCurveTo(
       x1, cy0,
       x1, cy1,
-      x1 - padh, y1 - padv
+      x1 - headPadH, y1 - headPadV
     )
     ctx.bezierCurveTo(
       cx1, y1,
       cx0, y1,
-      x0 + padh, y1 - padv)
+      x0 + headPadH, y1 - headPadV)
     ctx.bezierCurveTo(
       x0, cy1,
       x0, cy0,
-      x0 + padh, y0 + padv
+      x0 + headPadH, y0 + headPadV
     )
     ctx.closePath()
     ctx.stroke()
