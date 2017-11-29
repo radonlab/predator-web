82595c98c8e4fc5c573e966662e83449a08caea4
skylerlee <skyler.ac.lee@gmail.com>
2017-11-29 10:32:23 +0800

    Add http module

---
+++ src/core/http.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2017, Skyler.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+import axios from 'axios'
+
+const http = axios.create({
+})
+
+export default http
