3112861c53544e8ff37f55525294b30c87e06f9f
skylerlee <skyler.ac.lee@gmail.com>
2017-12-02 01:55:02 +0800

    Update parser

--- src/webport/SimpleParser.js
+++ src/webport/SimpleParser.js
@@ -4,9 +4,34 @@
  * found in the LICENSE file.
  */
 
+/**
+ * A simple parser for iflytek asr result
+ */
 class SimpleParser {
   constructor () {
+  }
 
+  parse (result) {
+    // tokenize
+    let tokens = this.tokenize(result.result)
+    console.log(tokens)
+  }
+
+  tokenize (dict) {
+    let tokens = []
+    for (let i = 0; i < dict.length; i++) {
+      let item = dict[i]
+      let ws = item.ws
+      for (let j = 0; j < ws.length; j++) {
+        let word = ws[j]
+        let cw = word.cw
+        for (let k = 0; k < cw.length; k++) {
+          let token = cw[k]
+          tokens.push(token)
+        }
+      }
+    }
+    return tokens
   }
 }
 
