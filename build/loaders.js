1c279702ed4844b0be7c82acd873ddab87572760
skylerlee <skyler.ac.lee@gmail.com>
2017-12-02 16:26:44 +0800

    Add static file loader

--- build/loaders.js
+++ build/loaders.js
@@ -9,6 +9,16 @@ function cssLoader (env) {
   }
 }
 
+function urlLoader (env) {
+  return {
+    loader: 'url-loader',
+    options: {
+      name: '[path][name].[ext]?v=[hash:8]',
+      limit: 1024 * 8
+    }
+  }
+}
+
 function get (env) {
   return [
     {
@@ -22,6 +32,10 @@ function get (env) {
     {
       test: /\.scss$/,
       use: ['style-loader', cssLoader(env), 'postcss-loader', 'sass-loader']
+    },
+    {
+      test: /\.(jpg|png|svg)$/,
+      use: [urlLoader(env)]
     }
   ]
 }
